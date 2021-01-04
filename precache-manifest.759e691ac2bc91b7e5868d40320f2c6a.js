bfcc12c4e9a659a42a4e77f737268362d1dbf50b
marsbtyne <mars.ballantyne@gmail.com>
2021-01-04 13:26:55 -0500

    Updates

---
+++ precache-manifest.759e691ac2bc91b7e5868d40320f2c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528ce72b46a6effae84888c3231b6fde",
+    "url": "/nycfridge/index.html"
+  },
+  {
+    "revision": "26c4b73d2b54a9ff91c5",
+    "url": "/nycfridge/static/css/main.a649c001.chunk.css"
+  },
+  {
+    "revision": "4447747c6167e493a9ef",
+    "url": "/nycfridge/static/js/2.fba2228d.chunk.js"
+  },
+  {
+    "revision": "1c327172e4036ad259b71fd0f56a0d37",
+    "url": "/nycfridge/static/js/2.fba2228d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c4b73d2b54a9ff91c5",
+    "url": "/nycfridge/static/js/main.60d170d8.chunk.js"
+  },
+  {
+    "revision": "044e5e273992d4a7d9c3",
+    "url": "/nycfridge/static/js/runtime-main.6bd1c121.js"
+  }
+]);
